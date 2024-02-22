c9e99ab54c39121e99af3463332498b454e0762f
shmurthy <shmurthy@fowler-lpw-dgx-0.chapman.edu>
2024-02-21 19:52:52 -0800

    Adjusting files

--- Assignment1/src/random_forest_model.py
+++ Assignment1/src/random_forest_model.py
@@ -22,7 +22,7 @@ X_test = unpickle_data("X_test.pkl")
 y_train = unpickle_data("y_train.pkl")
 y_test = unpickle_data("y_test.pkl")
 
-rfc = rfc(n_estimators=1, random_state=50)
+rfc = rfc(n_estimators=10, random_state=50)
 
 rfc.fit(X_train, y_train)
 
